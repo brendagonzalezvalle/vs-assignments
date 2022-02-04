7b9d3ecd364b5b191f6657f6c855392f761008b8
brendagonzalezvalle <brendagonzalezvalle@gmail.com>
2022-02-03 23:01:55 -0700

    Finalized functions/assignment.

--- node1/main.js
+++ node1/main.js
@@ -2,20 +2,19 @@ const readlineSync = require('readline-sync');
 const question = readlineSync.question("What is your name?")
 console.log('Hi ' + question);
 
-const operator = readlineSync.question("Please enter the operation to perform: add, sub, mul, div ?")
+const operator = readlineSync.question("Please enter the operation to perform: add, sub, mul, div? ")
 const number1 = readlineSync.question("Please enter your first number: ");
 console.log(typeof number1)
 
 const number2 = readlineSync.question("Please enter your second number: ");
-// console.log(num2)
+console.log(typeof number2)
 
-// console.log(sub(number1,number2))
 
 if (operator === "add" || operator === "Add" || operator === "ADD") {
     function add(num1, num2) {
         return Number(num1) + Number(num2)
     }
-
+    console.log("The result is:")
     console.log(add(number1, number2))
 }
 
@@ -24,41 +23,23 @@ else if (operator === "sub") {
     function sub(num1, num2) {
         return Number(num1) - Number(num2)
     }
+    console.log("The result is:")
+    console.log(sub(number1, number2))
 }
 
+else if (operator === "mul") {
+    function multiplies(num1, num2) {
+        return Number(num1) * Number(num2)
+    }
+    console.log("The result is:")
+    console.log(multiplies(number1, number2))
 
-
-
-
-function multiplies(num1, num2) {
-    return num1 * num2
 }
+else if (operator === "div") {
+    function divide(num1, num2) {
+        return Number(num1) / Number(num2)
+    }
+    console.log("The result is:")
+    console.log(divide(number1, number2))
 
-
-function divide(num1, num2) {
-    return num1 / num2
 }
-
-
-
-
-
-
-
-// function operatorChoice() {
-    // let operator = readline.question("Operator:");
-    // switch (operator){
-    //     case "add":
-    //         return operator;
-    //     case "sub":
-    //         return operator;
-    //     case "mul":
-    //         return operator;
-    //     case "div":
-    //         return operator;
-    //     default:
-    //         console.log("Please enter 'add', 'sub', 'mul', or 'div'!");
-    //     }
-    // }
-
-
