39cf1a8bd02b3ed48f631c5a0dd01bb81c5d438f
brendagonzalezvalle <brendagonzalezvalle@gmail.com>
2022-06-30 19:21:55 -0600

    added math.floor to allow random number to be provided.

--- colossal_adventure_rpg_project/app.js
+++ colossal_adventure_rpg_project/app.js
@@ -5,11 +5,18 @@ console.log(intro)
 const userName = readlineSync.question("What is your name? ");
 console.log("Great thank you " + userName + "!")
 
-let enemy = false
 
-while (true){
-    let userWalk = readlineSync.keyIn("Enter a (w) if you would like to walk through the tunnel?")
-    if(){
+let randomNumber = Math.floor(Math.random()*10)
+// let userWalk = readlineSync.keyIn("Enter a (w) to walk through the tunnel?")
+let enemy = 0
 
-    }
+while (enemy < 3 ){
+    enemy++
+//    console.log(randomNumber)
+   console.log("Keep going you are getting closer to the end of the tunnel!")
+   let userWalk = readlineSync.keyIn("Enter a (w) to walk through the tunnel?")
+    
+    continue
+    
 }
+
