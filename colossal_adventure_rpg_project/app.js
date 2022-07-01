4deefcf1b160a6193dd7d9a7aac12948aeb40842
brendagonzalezvalle <brendagonzalezvalle@gmail.com>
2022-07-01 17:06:24 -0600

    added a switch case removed while loop

--- colossal_adventure_rpg_project/app.js
+++ colossal_adventure_rpg_project/app.js
@@ -1,4 +1,8 @@
-// Tips: 1. write functions for actions that happen, 2 life span the players health points. Enemy array variable
+// Tips focus on: 1. write functions for actions that happen, 2 life span the players health points. "Enemy array variable array of objects"
+
+
+
+
 
 
 
@@ -10,41 +14,34 @@ const userName = readlineSync.question("What is your name? ")
 
 console.log("Great thank you " + userName + "!")
 
-// const userChoice = readlineSync.keyIn("Would you like to attack or run? Type (a) to attack or type (r) to run") 
-
-
-// let randomNumber = Math.floor(Math.random()*10)
-// let userWalk = readlineSync.keyIn("Enter a (w) to walk through the tunnel?")
-// let enemy = 0
-
-// const catEnemy = function (){
-//    return ("There is a cat coming to attack" + userChoice)
-// }
-
 
 function walk(){
-    let randomNumber = Math.random();
-    console.log(randomNumber)
+    let chance = Math.floor((Math.random()*2)+1);
+   
     console.log("Moving");
     let userChoice = readlineSync.keyIn("Would you like to attack or run? Type (a) to attack or type (r) to run") 
-    
-    while (randomNumber < 0.25){
-        console.log("It's working") 
-        if (randomNumber < .025 || randomNumber > .400 ){
-            let userChoice
-        }
-        else if (randomNumber ){
-            console.log("We got past it")
-        }        
+
+    switch(chance){
+        case 1:
+            encounter() 
         break;
-    }
+        default:
+            safe() 
+        break; 
+    
+
+
+    // while (randomNumber < 0.25){
+    //     console.log("It's working") 
+    //     if (randomNumber < .025 || randomNumber > .400 ){
+    //         let userChoice
+    //     }
+    //     else if (randomNumber ){
+    //         console.log("We got past it")
+    //     }        
+    //     break;
+    // } safe - safe place 
+    // encounter - enemies
 }
 
 walk()
-
-// while (enemy < 3 ){
-//     enemy++
-//    console.log(randomNumber)
-//    console.log("Keep going you are getting closer to the end of the tunnel!")
-//    let userWalk = readlineSync.keyIn("Enter a (w) to walk through the tunnel?") 
-    // / }
