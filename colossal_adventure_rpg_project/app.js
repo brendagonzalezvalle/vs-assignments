cda64fa7e4ea034e2ca883b35b61ddedbd4f9c50
brendagonzalezvalle <brendagonzalezvalle@gmail.com>
2022-07-11 16:59:11 -0600

    switched back to while loops

--- colossal_adventure_rpg_project/app.js
+++ colossal_adventure_rpg_project/app.js
@@ -1,10 +1,12 @@
 // Tips focus on: 1. write functions for actions that happen, 2 life span the players health points. "Enemy array variable array of objects"
 
 
+let defeated = false
+let hp = 100
 
 
 
-
+// Keep this line of code:  Intro/ stores name
 
 const readlineSync = require("readline-sync")
 const intro = readlineSync.question("Hi Welcome! Let's play a game today!! Wohooo!");
@@ -15,33 +17,52 @@ const userName = readlineSync.question("What is your name? ")
 console.log("Great thank you " + userName + "!")
 
 
-function walk(){
-    let chance = Math.floor((Math.random()*2)+1);
-   
-    console.log("Moving");
-    let userChoice = readlineSync.keyIn("Would you like to attack or run? Type (a) to attack or type (r) to run") 
 
-    switch(chance){
-        case 1:
-            encounter() 
-        break;
-        default:
-            safe() 
-        break; 
+while(hp > 0){
+    walk()
+    }
+
+// ********************************************************** walk calls
+// fight , then fight calls enemy encouter, enemy encounter calls , enemy attack
+// function safe(){
+//    readline.keyInPause("You are safe great job")
+// }
+// safe()
+
+// function fight(){
+
+// }
+
+function walk(){
+    const walking =readlineSync.keyIn("To start walking press(w)?")
+    let chance = Math.floor((Math.random()*2)+1);
+    readlineSync.keyInPause("Oh no get ready to begin fighting ")
+    let willFight = readlineSync.keyIn("(f) for fighting, (i) for checking inventory")
+        if(willWalk === "f"){
+            fight()
+        } else if (willWalk === "i"){
+            checkInventroy()
+        } 
+    }
+    
+    // console.log("Moving");
+    // let userChoice = readlineSync.keyIn("Would you like to attack or run? Type (a) to attack or type (r) to run") 
+    
+    
+// } 
+
+
+
+//     switch(chance){
+//         case 1:
+//             encounter() 
+//         break;
+//         default:
+//             safe() 
+//         break; 
     
 
 
-    // while (randomNumber < 0.25){
-    //     console.log("It's working") 
-    //     if (randomNumber < .025 || randomNumber > .400 ){
-    //         let userChoice
-    //     }
-    //     else if (randomNumber ){
-    //         console.log("We got past it")
-    //     }        
-    //     break;
-    // } safe - safe place 
-    // encounter - enemies
-}
-
-walk()
+   
+// } safe - safe place 
+// encounter - enemies
