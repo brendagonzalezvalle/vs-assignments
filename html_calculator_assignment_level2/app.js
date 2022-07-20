771ddc848ed6177c7d71d84cab36cb405e9d226f
brendagonzalezvalle <brendagonzalezvalle@gmail.com>
2022-07-19 20:10:08 -0600

    form 1 completed

--- html_calculator_assignment_level2/app.js
+++ html_calculator_assignment_level2/app.js
@@ -8,3 +8,28 @@
 // - You will inject the result into the HTML (not an alert)
 // - Your website will have 3 colors
 // - Your website will have proper padding/spacing to lay things out nicely
+
+const form1 = document["adding-form"]
+
+// Form 1
+
+form1.addEventListener("submit", function(e){
+    e.preventDefault()
+
+    const firstNumberAdding = form1.addNum1.value
+    const secondNumberAdding = form1.addNum2.value
+    
+    form1.addNum1.value = ""
+    form1.addNum2.value = ""
+    
+    console.log(firstNumberAdding)
+    console.log(secondNumberAdding)
+    
+    const h1 = document.createElement("h1")
+    h1.textContent = firstNumberAdding + " " + secondNumberAdding
+    document.getElementById("section1").append(h1)
+
+
+})
+
+// Form 2
