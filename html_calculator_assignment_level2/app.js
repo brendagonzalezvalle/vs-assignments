7ab9f46b2679528d401ac0a48daa73fed942e550
brendagonzalezvalle <brendagonzalezvalle@gmail.com>
2022-07-26 17:10:32 -0600

    created simple calculators

--- html_calculator_assignment_level2/app.js
+++ html_calculator_assignment_level2/app.js
@@ -1,4 +1,4 @@
-// You are going to create a simple calculator that can Add, Subtract, and Multiply.
+// done You are going to create a simple calculator that can Add, Subtract, and Multiply.
 
 // **Requirements:**
 
@@ -14,23 +14,31 @@ const form2 = document["subtracting-form"]
 const form3 = document["multiplying-form"]
 
 
+
+
 // Form 1 adding
 
-form1.addEventListener("submit", function(e){
+form1.addEventListener("submit", function (e) {
     e.preventDefault()
+
 
     const firstNumberAdding = form1.addNum1.value
     const secondNumberAdding = form1.addNum2.value
+    const result = ((firstNumberAdding*1) + (secondNumberAdding*1))
+
     
     form1.addNum1.value = " "
     form1.addNum2.value = " "
     
     console.log(firstNumberAdding)
     console.log(secondNumberAdding)
+
+
     
-    const header2 = document.createElement("h2")
-    h1.textContent = firstNumberAdding + " " + secondNumberAdding
-    document.getElementById("section1").append(header2)
+    const header1 = document.createElement("h2")
+    header1.textContent = result
+    document.getElementById("section1").append(header1)
+
 
 
 })
@@ -42,6 +50,7 @@ form2.addEventListener("submit", function(e){
 
     const firstNumberSubtracting = form2.subNum1.value
     const secondNumberSubtracting = form2.subNum2.value
+    const subractResult = ((firstNumberSubtracting*1) - (secondNumberSubtracting*1)) 
 
     form2.subNum1.value = " "
     form2.subNum2.value = " "
@@ -49,9 +58,9 @@ form2.addEventListener("submit", function(e){
     console.log(firstNumberSubtracting)
     console.log(secondNumberSubtracting)
 
-    const h2 = document.createElement("h2")
-    h2.textContent = firstNumberSubtracting + " " + secondNumberSubtracting
-    document.getElementById("section2").append(h2)
+    const header2 = document.createElement("h2")
+    header2.textContent = subractResult
+    document.getElementById("section2").append(header2)
 
 
 
@@ -64,6 +73,7 @@ form3.addEventListener("submit", function(e){
 
     const multiplyNumber1 = form3.mulNum1.value
     const multiplyNumber2 = form3.mulNum2.value
+    const multiplyingResult = ((multiplyNumber1*1) * (multiplyNumber2*1))
 
     form3.mulNum1.value = ""
     form3.mulNum2.value = ""
@@ -72,8 +82,8 @@ form3.addEventListener("submit", function(e){
     console.log(multiplyNumber1)
     console.log(multiplyNumber2)
 
-    const h2 = document.createElement("h2")
-    h2.textContent = multiplyNumber1 + " " + multiplyNumber2
-    document.getElementById("section3").append(h2)
+    const header3 = document.createElement("h2")
+    header3.textContent = multiplyingResult
+    document.getElementById("section3").append(header3)
 
 })
